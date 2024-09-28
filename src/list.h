99cc9e09b01e5a0a3390538d3a1c52faffd5cbd3
Vladislav Gusev <gysevvlad@gmail.com>
2024-09-28 16:06:43 +0300

    [ntt] disable internal tests, rename ntt_queue -> ntt_list

--- src/list.h
+++ src/list.h
@@ -4,22 +4,22 @@
 
 #include <stddef.h>
 
-typedef struct ntt_queue {
+typedef struct ntt_list {
   ntt_node_t node;
-} ntt_queue_t;
+} ntt_list_t;
 
-static inline void ntt_queue_init(ntt_queue_t *queue) {
+static inline void ntt_list_init(ntt_list_t *queue) {
   queue->node.next = &queue->node;
   queue->node.prev = &queue->node;
 }
 
-static inline int ntt_queue_empty(ntt_queue_t *queue) {
+static inline int ntt_list_empty(ntt_list_t *queue) {
   return queue->node.next == &queue->node;
 }
 
-static inline void ntt_queue_push_back(ntt_queue_t *queue, ntt_node_t *node,
-                                       int *first) {
-  *first = ntt_queue_empty(queue);
+static inline void ntt_list_push_back(ntt_list_t *queue, ntt_node_t *node,
+                                      int *first) {
+  *first = ntt_list_empty(queue);
 
   ntt_node_t *prev = &queue->node;
   ntt_node_t *next = queue->node.next;
@@ -30,7 +30,7 @@ static inline void ntt_queue_push_back(ntt_queue_t *queue, ntt_node_t *node,
   node->prev = prev;
 }
 
-static inline ntt_node_t *ntt_queue_pop_front(ntt_queue_t *queue, int *last) {
+static inline ntt_node_t *ntt_list_pop_front(ntt_list_t *queue, int *last) {
   ntt_node_t *node = queue->node.prev;
   ntt_node_t *prev = node->prev;
   ntt_node_t *next = node->next;
@@ -38,7 +38,7 @@ static inline ntt_node_t *ntt_queue_pop_front(ntt_queue_t *queue, int *last) {
   prev->next = next;
   next->prev = prev;
 
-  *last = ntt_queue_empty(queue);
+  *last = ntt_list_empty(queue);
 
   return node != &queue->node ? node : NULL;
 }
--- src/task_queue_impl.h
+++ src/task_queue_impl.h
@@ -1,18 +1,18 @@
 #pragma once
 
-#include "./queue.h"
+#include "./list.h"
 #include "./task_inl.h"
 
 #include <threads.h>
 
 typedef struct ntt_task_queue {
-  ntt_queue_t queue;
+  ntt_list_t queue;
   mtx_t mtx;
   cnd_t cnd;
 } ntt_task_queue_t;
 
 static inline void ntt_task_queue_init_impl(ntt_task_queue_t *self) {
-  ntt_queue_init(&self->queue);
+  ntt_list_init(&self->queue);
   int rc = mtx_init(&self->mtx, mtx_plain);
   assert(rc == thrd_success);
   cnd_init(&self->cnd);
@@ -22,7 +22,7 @@ static inline void ntt_task_queue_push_back_impl(ntt_task_queue_t *self,
                                                  ntt_task_node_t *task,
                                                  int *first) {
   mtx_lock(&self->mtx);
-  ntt_queue_push_back(&self->queue, &task->node, first);
+  ntt_list_push_back(&self->queue, &task->node, first);
   cnd_signal(&self->cnd);
   mtx_unlock(&self->mtx);
 }
@@ -30,10 +30,10 @@ static inline void ntt_task_queue_push_back_impl(ntt_task_queue_t *self,
 static inline ntt_task_node_t *
 ntt_task_queue_pop_front_blocking_impl(ntt_task_queue_t *self, int *last) {
   mtx_lock(&self->mtx);
-  ntt_node_t *node = ntt_queue_pop_front(&self->queue, last);
+  ntt_node_t *node = ntt_list_pop_front(&self->queue, last);
   while (node == NULL) {
     cnd_wait(&self->cnd, &self->mtx);
-    node = ntt_queue_pop_front(&self->queue, last);
+    node = ntt_list_pop_front(&self->queue, last);
   }
   mtx_unlock(&self->mtx);
   return ntt_container_of(node, ntt_task_node_t, node);
